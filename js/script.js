1fff6a98f1f1f913d7acd1bf2c01f922a4db8809
Krzysztof Olma <olmak3910@gmail.com>
2018-01-05 21:36:16 +0100

    Exercise_8_4

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
+// linear comment
+
+// Below I have commented on a function call to disable it
+// console.log('The commented will not be called');
+
+/*
+block comment
+console.log('The commented will not be called');
+*/
